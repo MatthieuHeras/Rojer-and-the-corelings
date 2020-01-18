7cd8ea8c87694eb76f4a37c68670a72a06c6a9b3
Matthieu Heras <37445049+MatthieuHeras@users.noreply.github.com>
2020-01-18 15:36:05 +0100

    Add Cam controls
    - Modify inputs (azerty)
    - Add CameraController scipt

---
+++ Rojer and the corelings/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float zoomSpeed = 5f;
+    [SerializeField] private Camera cam;
+
+    private void Start()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("Missing component Camera reference on : " + gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, 3f, 20f);
+    }
+}
